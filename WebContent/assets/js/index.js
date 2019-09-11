c788d7749a8c0d2aa234ff5d805f896a7053b03f
Pablo Ferreiro <pferreiromero@gmail.com>
2019-09-11 21:15:05 +0200

    WIP New Main Menu

--- WebContent/assets/js/index.js
+++ WebContent/assets/js/index.js
@@ -36,30 +36,46 @@ function login() {
 	x.style.display = "none";
   var y = document.getElementById("main");
 	y.style.display = "block";
-  getid();
-}
-
-function getid(){
-	loadJSON(whole_url + "/emby/Users/Public",
+  loadJSON(whole_url + "/emby/Users/Public",
          function(data){
-					 id = data[0].Id; // At the moment the code can only check your Id in section 0
-					 getmovies();
-				 },
+           id = data[0].Id; // At the moment the code can only check your Id in section 0
+         },
          function(xhr) { console.error(xhr); }
 );
+}
+
+
+function main(clicked_id){
+  var x = document.getElementById("main");
+  x.style.display = "none";
+  var y = document.getElementById("items");
+  y.style.display = "block";
+  console.log("Button clicked: " + clicked_id);
+  if (clicked_id = "movies"){
+    getmovies();
+  }
 }
 
 function getmovies(){
 	loadJSON(whole_url + "/emby/Users/" + id + "/Items?api_key=" + api + "&Recursive=true&IncludeItemTypes=Movie",
          function(data){
+           movies = data;
 					 var i = 0;
 					 while (i < data.Items.length) {
-
 						 console.log(i + " " + data.Items[i].Name);
-						 document.getElementById('main').innerHTML +="<a href=" + whole_url + "/emby/Videos/" + data.Items[i].Id + "/stream.mp4><img src=" + whole_url + "/emby/Items/" + data.Items[i].Id + "/Images/Primary width=180 height=270>";
+						 document.getElementById("items").innerHTML += "<a href='javascript:videoplayer(movies.Items[" + i +"].Id)''>" + "<img src=" + whole_url + "/emby/Items/" + data.Items[i].Id + "/Images/Primary width=180 height=270>";
 						 i++;
 					 }
 				 },
          function(xhr) { console.error(xhr); }
 );
+}
+
+function videoplayer(id_video){
+  console.log(id_video);
+  var x = document.getElementById("items");
+  x.style.display = "none";
+  var y = document.getElementById("player");
+  y.style.display = "block";
+  //document.getElementById('vid').source = whole_url + "/emby/Videos/" + id + "/stream.mp4";
 }
